dd16994aea601ece302644e07258eedfa5e944c9
JackHallisy <39491444+JackHallisy@users.noreply.github.com>
2018-10-26 12:58:22 -0400

    Add files via upload

---
+++ Controllers/RandomDateController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RndDateMVC.Controllers
+{
+    public class RandomDateController : Controller
+    {
+        // GET: RandomDate
+        public ActionResult Index()
+        {
+            ViewBag.RandomDate = RandomDay();
+
+            return View();
+        }
+
+
+        public DateTime RandomDay()
+        {
+            Random gen = new Random();
+            DateTime start = new DateTime(1995, 1, 1);
+            int range = (DateTime.Today - start).Days;
+            return start.AddDays(gen.Next(range));
+        }
+    }
+}
